cb7127c916e821a40035e0c040447f278d21f401
Anant Dubey <anantkumar312@gmail.com>
2024-01-16 11:27:58 +0530

    feat: show dummy category grid

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
@@ -10,17 +13,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick your category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            "Hello",
-            style: TextStyle(color: Colors.white),
-          ),
+        children: [
+          // alternative to below code: availableCategories.map((category) => CategoryGridItem(category: category)).toList();
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
