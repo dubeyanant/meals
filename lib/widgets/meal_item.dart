dafbd1e77ec417c4ea4a521b09981da9823325bc
Anant Dubey <anantkumar312@gmail.com>
2024-01-16 14:58:13 +0530

    feat: add meta information about meal

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 
 import 'package:transparent_image/transparent_image.dart';
 
@@ -8,6 +9,16 @@ class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complextyText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +59,22 @@ class MealItem extends StatelessWidget {
                   ),
                   const SizedBox(height: 12),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(meal.title),
+                      MealItemTrait(
+                        icon: Icons.schedule,
+                        label: '${meal.duration} min',
+                      ),
+                      const SizedBox(width: 12),
+                      MealItemTrait(
+                        icon: Icons.play_circle_fill_outlined,
+                        label: complextyText,
+                      ),
+                      const SizedBox(width: 12),
+                      MealItemTrait(
+                        icon: Icons.attach_money,
+                        label: affordabilityText,
+                      ),
                     ],
                   ),
                 ],
